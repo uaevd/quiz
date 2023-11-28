b2efde12933fefaccf5fc032b9384267dbb23480
uaevd <saekkigae45@gmail.com>
2023-11-28 21:31:53 +0700

    :recycle: Refactor `QuizOptions` organism
    - Reorder `InputForm` molecule and rewrite label

--- src/components/organisms/QuizOptions.tsx
+++ src/components/organisms/QuizOptions.tsx
@@ -19,10 +19,10 @@ interface Props {
 export const QuizOptions = ({ categories, difficulties }: Props) => (
     <Container>
         <InputForm label="Category" inputType={<SelectItems items={categories} />} />
+        <InputForm label="Difficulty" inputType={<SelectItems items={difficulties} />} />
         <InputForm
-            label="Problems Count"
+            label="Problem Count"
             inputType={<InputNumber value="10" onChange={console.log} />}
         />
-        <InputForm label="Difficulty" inputType={<SelectItems items={difficulties} />} />
     </Container>
 );
