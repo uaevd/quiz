371b80b9214bc96c1f12b9af863cb7b8384187f0
uaevd <saekkigae45@gmail.com>
2023-11-29 06:34:06 +0700

    :building_construction: Reorganize `QuizOptions` and `QuizSetup`
    - Move `StartButton` atom from `QuizSetup` to `QuizOptions`

--- src/components/organisms/QuizOptions.tsx
+++ src/components/organisms/QuizOptions.tsx
@@ -2,14 +2,21 @@ import styled from '@emotion/styled';
 
 import { InputNumber } from 'components/atoms/InputNumber';
 import { SelectItems } from 'components/atoms/SelectItems';
+import { StartButton } from 'components/atoms/StartButton';
 import { InputForm } from 'components/molecules/InputForm';
 import { OptionItem } from 'types/OptionItem';
 
 const Container = styled.div`
+    align-items: stretch;
+    background-color: #fff;
+    border-radius: 20px;
+    box-shadow: 0 4 20px rgba(0, 0, 0, 0.2);
     display: flex;
     flex-direction: column;
+    flex-basis: 500px;
     justify-content: center;
-    width: 100%;
+    margin: 20px;
+    padding: 40px;
 `;
 
 interface Props {
@@ -31,5 +38,6 @@ export const QuizOptions = ({ categories, difficulties }: Props) => (
             label="Problem Count"
             inputElement={<InputNumber value="10" onChange={console.log} />}
         />
+        <StartButton />
     </Container>
 );
